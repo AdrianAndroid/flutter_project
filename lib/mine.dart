eb78aa591acf0141c8fe294718d6b022f524d938
zhaojian2 <zhaojian2@joyy.com>
2021-12-31 15:03:14 +0800

    准备研究TextField

--- lib/mine.dart
+++ lib/mine.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/smscode.dart';
 
-class Mine extends StatelessWidget {
+class Mine extends StatefulWidget {
   const Mine();
 
   @override
+  State<Mine> createState() => _MineState();
+}
+
+class _MineState extends State<Mine> {
+  @override
   Widget build(BuildContext context) {
     return Column(children: [
-      Expanded(flex: 1, child: Center(child: Text("mine"))),
+      Expanded(flex: 1, child: Center(child: _buildSmsCode())),
       SizedBox(
           height: 100,
           child: Row(
@@ -26,5 +32,46 @@ class Mine extends StatelessWidget {
             ],
           ))
     ]);
+  }
+
+  VerificationController _verificationController = VerificationController();
+
+  Widget _buildSmsCode() {
+    return Container(
+      child: VerificationBox(
+        textNormalStyle: TextStyle(
+          color: new Color(0xFF1B1B26),
+          fontSize: 24,
+          fontWeight: FontWeight.normal,
+          letterSpacing: -0.6,
+        ),
+        verificationController: _verificationController,
+        showCursor: true,
+        itemWidth: 44,
+        autoFocus: true,
+        cursorWidth: 2,
+        cursorColor: Color(0xFFFF265C),
+        cursorIndent: 10,
+        cursorEndIndent: 10,
+        borderRadius: 2,
+        borderWidth: 0.5,
+        bgColor: Color(0xFFF5F5FA),
+        type: VerificationBoxItemType.box,
+        focusBorderColor: Color(0xFFE1E1E6),
+        onSubmitted: (value) async {
+          // viewModel.onSubmitted(
+          //     value, widget.phoneNum, widget.phoneCode, context,
+          //     callback: widget.loginCallBack);
+        },
+        errorColor: Colors.yellow,
+        errorTextStyle: TextStyle(
+          color: Color(0xFFFF265C),
+          fontSize: 24,
+          fontWeight: FontWeight.normal,
+          letterSpacing: -0.6,
+        ),
+        borderColor: Colors.transparent,
+      ),
+    );
   }
 }
