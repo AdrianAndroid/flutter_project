4703d45e8399358fd9095597522f725d2d85b8b7
flannery <18310579837@163.com>
2022-01-18 16:15:50 +0800

    ChangeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,64 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class Counter extends ChangeNotifier {
+  // 这里也可以使用with来进行实现
+  int _count = 0; //数值计算
+  int get count => _count;
+
+  addCount() {
+    _count++;
+    notifyListeners();
+  }
+}
+
+Counter _counter = Counter();
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    _counter.addListener(() {
+      // 数值改变的监听
+      print('YM--->新数值:${_counter.count}');
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _counter.dispose(); //移除监听
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Hello')),
-      body: Center(child: Text('Content')),
+      body: Center(
+        child: Column(
+          children: [
+            RaisedButton(
+              onPressed: () {
+                // disposediao
+                _counter.dispose();// dispose之后就不能收到
+                print('YM->dispose()');
+              },
+              child: Text('dispose'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                _counter.addCount();
+              },
+              child: Text('计数'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
