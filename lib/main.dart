ac70d32db37ccf424d21ddbf9fff2877634bb212
zhaojian2 <zhaojian2@joyy.com>
2022-01-20 15:33:51 +0800

    _Comsumer工具类

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,11 @@ class Item {
 class CartModel extends ChangeNotifier {
   // 用于保存购物车中商品列表
   final List<Item> _items = [];
-
   // 禁止改变购物车里的商品信息
   UnmodifiableListView<Item> get items => UnmodifiableListView(_items);
-
   // 购物车中商品的总价
   double get totalPrice =>
       _items.fold(0, (value, item) => value + item.count + item.price);
-
   // 将[item]添加到购物车。这里唯一一种可能从外部改变购物车的方法
   void add(Item item) {
     _items.add(item);
@@ -30,11 +27,9 @@ class CartModel extends ChangeNotifier {
     notifyListeners();
   }
 }
-
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -47,10 +42,24 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class ProviderRoute extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _PreviderRouteState();
+}
+
+// 这是一个便捷类，会获得当前context和指定数据类型的Provider
+class _Consumer<T> extends StatelessWidget {
+
+  final Widget Function(BuildContext context, T? value) builder;
+
+  const _Consumer({Key? key,required this.builder}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return builder(
+      context, context.watch<T>()
+    );
+  }
 }
 
 class _PreviderRouteState extends State<ProviderRoute> {
@@ -63,10 +72,20 @@ class _PreviderRouteState extends State<ProviderRoute> {
               return Column(
                 children: [
                   Text('Hi~'),
-                  Builder(builder: (context) {
-                    var cart = context.watch<CartModel>();
-                    return Text("Hello cart => ${cart.totalPrice}");
-                  }),
+                  // 下面可以做如下优化
+                  // 1. 需要显示调用ChangeNotifierProvider.of,
+                  //    当APP内部依赖CartModel很多时，这样的代码将会很冗余
+                  // 2. 语义不明确：由于ChangeNotifierProvider是订阅者，那么依赖CartModel
+                  //    的Widget自然就是订阅者，其实也就是状态的消费者，如果我们用Builder
+                  //    来构建，语义就不是很明确；如果我们能使用一个具有明确意义的Widget，比如就叫Consumer
+                  //    ，这样最终的代码语义将会很明确，只要看到使用Consumer，我们就知道它是依赖某个跨组件或全局的状态。
+                  // Builder(builder: (context) {
+                  //   var cart = context.watch<CartModel>();
+                  //   return Text("Hello cart => ${cart.totalPrice}");
+                  // }),
+                  _Consumer<CartModel>(
+                    builder: (context, cart) => Text("总价:${cart?.totalPrice}"),
+                  ),
                   Builder(builder: (context) {
                     print('ElevatedButton build');
                     return ElevatedButton(
