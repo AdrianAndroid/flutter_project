0ccde6b973191110aff7550883b8492392f4f50e
zhaojian2 <zhaojian2@joyy.com>
2022-01-21 10:57:43 +0800

    FutureProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,50 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider/single_child_widget.dart';
 
-class TestProviderModel with ChangeNotifier, DiagnosticableTreeMixin {
-  int _number = 0;
+void main() => runApp(MyApp());
 
-  int get number => _number;
+// class Home {
+//   final String city = "Portlan";
+//
+//   Future<String> get fetchAddress {
+//     final address = Future.delayed(Duration(seconds: 2), () {
+//       return '1234 North Commercial Ave.';
+//     });
+//     return address;
+//   }
+// }
 
-  set number(int value) {
-    _number = value;
-    notifyListeners();
-  }
-
-  void addNumber() {
-    _number++;
-    notifyListeners();
-  }
-}
-
-class UserModel {
-  String name;
-  String userID;
-  bool isAuthor;
-  bool isVIP;
-
-  UserModel(this.name, this.userID, this.isAuthor, this.isVIP);
-}
-
-class UserProviderModel with ChangeNotifier, DiagnosticableTreeMixin {
-  UserModel _user;
-
-  UserProviderModel(this._user);
-
-  set user(UserModel value) {
-    _user = value;
-    notifyListeners();
-  }
-}
-
-void main() {
-  List<SingleChildWidget> providerList = [
-    ChangeNotifierProvider(create: (_) => TestProviderModel()),
-    ChangeNotifierProvider(
-      create: (_) => UserProviderModel(UserModel("cy", "1", true, true)),
-    )
-  ];
-
-  runApp(
-    MultiProvider(
-      providers: providerList,
-      child: TestProvider(),
-    ),
-  );
-}
-
-class TestProvider extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _TestProviderState();
-}
-
-class _TestProviderState extends State<TestProvider> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(title: Text('测试provider')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ChildOne(),
-              ChildTwo(),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            context.read<TestProviderModel>().addNumber();
-          },
-        ),
+        appBar: AppBar(title: Text('Material App Bar')),
+        body: HomeBody(),
       ),
     );
   }
 }
 
-class ChildOne extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _ChildOneState();
-}
-
-class ChildTwo extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _ChildTwoState();
-}
-
-class _ChildOneState extends State<ChildOne> {
+class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('1build');
-    return Container(
-      child: Text('1number = ${context.watch<TestProviderModel>()._number}'),
+    return Center(
+      child: FutureProvider(
+        create: (context) => Future.delayed(Duration(seconds: 2), ()=>'1234 '
+            'North Commercial Ave.'),
+        initialData: '数据正在初始化...',
+        child: ChildBody(),
+      ),
     );
   }
 }
 
-class _ChildTwoState extends State<ChildTwo> {
+class ChildBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('2build');
-    return Container(
-      child: Consumer2<TestProviderModel, UserProviderModel>(
-        builder: (context, testProvider, userProvider, child) {
-          return Text(
-              'testProviderNumber:${testProvider.number}\n用户:${userProvider._user.name}');
-        },
-      ),
-    );
+    return Container(child: Text('${context.watch<String>()}'));
   }
 }
