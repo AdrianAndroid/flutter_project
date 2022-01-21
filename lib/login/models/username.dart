e5c5da27b3c87b1465dd2f35bc8661570450e982
zhaojian2 <zhaojian2@joyy.com>
2022-01-21 18:38:08 +0800

    登陆逻辑

--- lib/login/models/username.dart
+++ lib/login/models/username.dart
@@ -9,6 +9,6 @@ class Username extends FormzInput<String, UsernameValidationError> {
 
   @override
   UsernameValidationError? validator(String value) {
-    return value?.isNotEmpty == true ? null : UsernameValidationError.empty;
+    return value.isNotEmpty == true ? null : UsernameValidationError.empty;
   }
 }
