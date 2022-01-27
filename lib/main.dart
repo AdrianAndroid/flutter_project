c99b83670e7a82af54345d9bdc1e9edfcb2bd013
zhaojian2 <zhaojian2@joyy.com>
2022-01-27 17:35:54 +0800

    FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
 
-// 第一种
-class Singleton1 {
-  Singleton1._privateConstructor();
-
-  static final Singleton1 _instance = Singleton1._privateConstructor();
-
-  factory Singleton1() {
-    return _instance;
-  }
-}
 
 void main() {
-  print('---first---');
-  Singleton1 s1 = Singleton1();
-  Singleton1 s2 = Singleton1();
-  print(s1 == s2);
 }
