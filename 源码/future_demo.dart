609dbf5fb5ac32ba3aa45a5c16cf423c06cfdb77
zhaojian2 <zhaojian2@joyy.com>
2022-02-08 11:33:07 +0800

    Example:synchronous functions

--- 源码/future_demo.dart
+++ 源码/future_demo.dart
@@ -1,21 +1,13 @@
-// String createOrderMessage() {
-//   var order = fetchUserOrder();
-//   return 'Your order is : $order';
-// }
-// Future<String> fetchUserOrder() =>
-//     // Imagine that this function is more complex and slow.
-//     Future.delayed(const Duration(seconds: 2), () => 'Large Latte');
-
-Future<void> fetchUserOrder2() {
-  // Imagine that this function is fetching user info from another service or
-  // database
-  return Future.delayed(Duration(seconds: 2), () => print('Large Latte'));
+String createOrderMessage() {
+  var order = fetchUserOrder();
+  return 'Your order is : $order';
 }
+Future<String> fetchUserOrder() =>
+    // Imagine that this function is more complex and slow.
+    Future.delayed(const Duration(seconds: 2), () => 'Large Latte');
 
 // 这种用法其实是错误的。
-main() {
-  print('Future main start...');
-  var f = fetchUserOrder2();
-  // print('Future result = $f'); // 使用返回值，就阻塞了
-  print('Future main end...');
+Future<void> main() async {
+  print('Fetching user order...');
+  print(createOrderMessage());
 }
