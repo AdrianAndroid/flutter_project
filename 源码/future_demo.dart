15ad01c64e05330d942184f99729462eecedd264
zhaojian2 <zhaojian2@joyy.com>
2022-02-08 11:55:55 +0800

    Example: async and await with try-catch

--- 源码/future_demo.dart
+++ 源码/future_demo.dart
@@ -1,25 +1,22 @@
 Future<void> printOrderMessage() async {
-  print('Awaiting user order...');
-  var order = await fetchUserOrder();
-  print('Your order is: $order');
+  try {
+    print('Awaiting user orer..');
+    var order = await fetchUserOrder();
+  } catch (err) {
+    print('Caught error: $err');
+  }
 }
 
 Future<String> fetchUserOrder() {
-  // Imagine that this function is more complex and slow.
-  return Future.delayed(Duration(seconds: 4), () => 'Large Latte');
+  // Imagine that this function is more complex.
+  var str = Future.delayed(
+      Duration(seconds: 4),
+      () => throw 'Cannot locate '
+          'user order');
+  return str;
 }
 
+// Handling errors
 Future<void> main() async {
-  countSeconds(4);
-  print('printOrderMessage start ...');
-  await printOrderMessage(); // 这个是阻塞的
-  print('printOrderMessage end ...');
-}
-
-void countSeconds(int s) {
-  for (var i = 1; i <= s; i++) {
-    print('countSecond $i start ...');
-    Future.delayed(Duration(seconds: 1), () => print(i));
-    print('countSecond $i end ...');
-  }
+  await printOrderMessage();
 }
