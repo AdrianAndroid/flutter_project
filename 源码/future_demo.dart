72e7da3f9d94098ed4ffa293c0ead0eb13875862
zhaojian2 <zhaojian2@joyy.com>
2022-02-08 11:27:12 +0800

    一个正确的例子.

--- 源码/future_demo.dart
+++ 源码/future_demo.dart
@@ -1,16 +1,20 @@
+// String createOrderMessage() {
+//   var order = fetchUserOrder();
+//   return 'Your order is : $order';
+// }
+// Future<String> fetchUserOrder() =>
+//     // Imagine that this function is more complex and slow.
+//     Future.delayed(const Duration(seconds: 2), () => 'Large Latte');
 
-String createOrderMessage() {
-  var order = fetchUserOrder();
-  return 'Your order is : $order';
+Future<void> fetchUserOrder2() {
+  // Imagine that this function is fetching user info from another service or
+  // database
+  return Future.delayed(Duration(seconds: 2), () => print('Large Latte'));
 }
-
-Future<String> fetchUserOrder() =>
-    // Imagine that this function is more complex and slow.
-    Future.delayed(const Duration(seconds: 2), () => 'Large Latte');
 
 // 这种用法其实是错误的。
 main() {
   print('Future main start...');
-  print(createOrderMessage());
+  fetchUserOrder2();
   print('Future main end...');
 }
