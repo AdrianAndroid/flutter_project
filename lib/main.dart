c011eaad3640263c3ccf8ae5388a612c6f6332f2
zhaojian2 <zhaojian2@joyy.com>
2022-02-09 09:45:01 +0800

    添加基础实施

--- lib/main.dart
+++ lib/main.dart
@@ -10,86 +10,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Woolha.com Flutter Tutorial',
-      home: FutureBuilderExample(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-Future<String> getValue() async {
-  await Future.delayed(Duration(seconds: 3));
-  return 'Woolha';
-}
-
-class FutureBuilderExample extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _FutureBuilderExampleState();
-}
-
-class _FutureBuilderExampleState extends State<FutureBuilderExample> {
-  late Future<String> _value;
-
-  @override
-  void initState() {
-    super.initState();
-    _value = getValue();
-  }
-
-  String _title = 'Hi';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Woolha.com Flutter Tutorial')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _title = _title + ".";
-          });
-        },
-        child: Icon(Icons.title),
-      ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Center(
-          child: FutureBuilder<String>(
-            future: _value,
-            initialData: 'App Name',
-            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-              print('State: ${snapshot.connectionState}');
-              print('State is Active ${ConnectionState.active == snapshot.connectionState}');
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircularProgressIndicator(),
-                    Visibility(
-                      child: Text(
-                        snapshot.data ?? "NULL",
-                        style: TextStyle(color: Colors.black, fontSize: 24),
-                      ),
-                    ),
-                  ],
-                );
-              } else if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return const Text('Error');
-                } else if (snapshot.hasData) {
-                  return Text(
-                    snapshot.data ?? 'NULL',
-                    style: const TextStyle(color: Colors.teal, fontSize: 36),
-                  );
-                } else {
-                  return const Text('Empty data');
-                }
-              } else {
-                return Text('State: ${snapshot.connectionState}');
-              }
-            },
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('多语言/国际化'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          brightness: Brightness.dark,
+          //automaticallyImplyLeading: false,
+          leading: Icon(Icons.add),
+        ),
+        body: Center(
+          child: Text('Hello World!'),
         ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
