5a60e5b754b2f92d77331c4ecfab47c7fafdb0dd
zhaojian2 <zhaojian2@joyy.com>
2022-02-09 13:44:58 +0800

    先注释掉

--- lib/l10n/localization_intl.dart
+++ lib/l10n/localization_intl.dart
@@ -2,11 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 
 class DemoLocalizations {
-  static Future<DemoLocalizations> load(Locale locale) {
-    final String name = locale.countryCode?.isEmpty == true
-        ? locale.languageCode
-        : locale.toString();
-    final String localName = Intl.canonicalizedLocale(name);
-    return initializeM
-  }
+  // static Future<DemoLocalizations> load(Locale locale) {
+  //   final String name = locale.countryCode?.isEmpty == true
+  //       ? locale.languageCode
+  //       : locale.toString();
+  //   final String localName = Intl.canonicalizedLocale(name);
+  //   return initialize
+  // }
 }
