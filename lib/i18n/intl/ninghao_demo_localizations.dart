d6a07209c0b65df3b9f4be14fcfdd1ee87701dad
zhaojian2 <zhaojian2@joyy.com>
2022-02-09 16:08:01 +0800

    arb文件 和 dart文件 相互转换

--- lib/i18n/intl/ninghao_demo_localizations.dart
+++ lib/i18n/intl/ninghao_demo_localizations.dart
@@ -1,7 +1,7 @@
 import 'package:intl/intl.dart';
 
 // 宁皓网教程
-// flutter pub run intl_translation:extract_to_arb
+// flutter pub pub run intl_translation:extract_to_arb
 // --output-dir=lib/demo/i18n/intl/
 // lib/demo/i18n/intl/ninghao_demo_localizations.dart
 // 自己摸索
@@ -20,3 +20,11 @@ class NinghaoDemoLocalizations {
         args: [name],
       );
 }
+
+// 通过arb反过来生成dart文件
+// flutter pub pub run intl_translation:generate_from_arb
+// --generated-file-prefix=ninghao_demo_ --output-dir=lib/demo/i18n/intl
+// --no-use-deferred-loading lib/demo/i18n/intl/ninghao_demo_localizations.dart
+// lib/demo/i18n/int/intl_*.arb
+// 自己摸索
+//flutter pub run intl_generator:generate_from_arb --generated-file-prefix=ninghao_demo_ --output-dir=lib/i18n/intl/ --no-use-deferred-loading lib/i18n/intl/ninghao_demo_localizations.dart lib/i18n/intl/intl_*.arb
