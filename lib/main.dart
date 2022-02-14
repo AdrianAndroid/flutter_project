39570497a3503e2ebe93b041dd8dab3794f121bc
zhaojian2 <zhaojian2@joyy.com>
2022-02-14 17:40:57 +0800

    类似于PopupWindow的弹窗

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,8 @@ class MyPageState extends State<MyPage> {
           children: [
             ElevatedButton(
               onPressed: () {
-                Toast.show(context: context, message: '弹出了Toast！！！');
+                //Toast.show(context: context, message: '弹出了Toast！！！');
+                showWeixinButtonView();
               },
               child: Text('第一个点击'),
             ),
@@ -56,4 +57,58 @@ class MyPageState extends State<MyPage> {
       ),
     );
   }
+
+  OverlayEntry? weixinOverlayEntry;
+
+  /// 展示微信下拉的弹窗
+  void showWeixinButtonView() {
+    weixinOverlayEntry = OverlayEntry(
+      builder: (context) {
+        return Positioned(
+          top: 10,
+          right: 20,
+          height: 320,
+          width: 200,
+          child: SafeArea(
+            child: Material(
+              color: Colors.black,
+              child: Column(
+                children: [
+                  _ExpandedItem('发起群聊', () {
+                    weixinOverlayEntry?.remove();
+                  }),
+                  _ExpandedItem('添加朋友', () {
+                    weixinOverlayEntry?.remove();
+                  }),
+                  _ExpandedItem('扫一扫', () {
+                    weixinOverlayEntry?.remove();
+                  }),
+                  _ExpandedItem('首付款', () {
+                    weixinOverlayEntry?.remove();
+                  }),
+                  _ExpandedItem('帮助与反馈', () {
+                    weixinOverlayEntry?.remove();
+                  }),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+    Overlay.of(context)?.insert(weixinOverlayEntry!);
+  }
+
+  _ExpandedItem(String title, GestureTapCallback callback) => GestureDetector(
+        onTap: callback,
+        child: Expanded(
+          child: ListTile(
+            leading: Icon(Icons.add, color: Colors.white),
+            title: Text(
+              title,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      );
 }
