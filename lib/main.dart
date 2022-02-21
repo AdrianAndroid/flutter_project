e6cafbba800f54d455e52fbd3a3649c31fc09a9a
zhaojian2 <zhaojian2@joyy.com>
2022-02-21 14:15:22 +0800

    分享方法

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+// Directionality
+// 用于确定文本和文字方向
+// 继承关系： Object > Diagnosticable > DiagnosticableTree > Widget > ProxyWidget
+// > InheritedWidget > Directionality
 
-class MyApp extends StatelessWidget {
+void main() => runApp(OrgApp());
+
+class OrgApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyApp();
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextDirection textDirection = TextDirection.ltr;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Woolha.com Flutter Tutorial',
       home: Scaffold(
         appBar: AppBar(title: Text('StreamSubscription')),
-        body: Center(
-          child: Text('看源码文件夹'),
+        body: Container(
+          width: double.infinity,
+          padding: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+          color: Colors.white,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          textDirection = TextDirection.ltr;
+                        });
+                      },
+                      child: Text('ltr')),
+                  ElevatedButton(
+                      onPressed: () {
+                        textDirection = TextDirection.rtl;
+                      },
+                      child: Text('rtl')),
+                ],
+              ),
+              Container(
+                color: Colors.yellow,
+                width: double.infinity,
+                child: Directionality(
+                  textDirection: textDirection,
+                  child: Container(
+                    width: double.infinity,
+                    child: Text(
+                      "演示Directionality",
+                      style: TextStyle(fontSize: 12.0),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
     );
-  }
-
-  _buildButton(String title, VoidCallback onPress) {
-    return ElevatedButton(onPressed: onPress, child: Text(title));
   }
 }
