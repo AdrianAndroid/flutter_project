a581e703f5961c884ac5b4cc8d4bf1dc73dec617
zhaojian2 <zhaojian2@joyy.com>
2022-02-23 17:31:10 +0800

    GestureDetecotor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,34 +11,52 @@ class MyApp extends StatelessWidget {
       title: 'Listener',
       home: Scaffold(
         appBar: AppBar(title: Text('手势检测')),
-        body: _Scale(),
+        body: _GestureRecognizer(),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class _Scale extends StatefulWidget {
-  const _Scale({Key? key}) : super(key: key);
-
+class _GestureRecognizer extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _ScaleState();
+  State<StatefulWidget> createState() => _GestureRecognizerState();
 }
 
-class _ScaleState extends State<_Scale> {
-  double _width = 200.0; // 通过修改图片宽度来达到缩放效果
+class _GestureRecognizerState extends State<_GestureRecognizer> {
+  TapGestureRecognizer _tapGestureRecognizer = TapGestureRecognizer();
+  bool _toggle = false; //变色开关
+
+  @override
+  void dispose() {
+    // 用到GestureRecognizer的话一定要调用其dispose方法释放资源
+    _tapGestureRecognizer.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: GestureDetector(
-        // 制定宽度，高度自适应
-        child: Image.asset("./images/sea.jpg", width: _width),
-        onScaleUpdate: (ScaleUpdateDetails details) {
-          setState(() {
-            // 缩放倍数在0.8到10之间
-            _width = 200 * details.scale.clamp(.8, 10.0);
-          });
-        },
+      child: Text.rich(
+        TextSpan(
+          children: [
+            TextSpan(text: "你好世界"),
+            TextSpan(
+              text: "点我变色",
+              style: TextStyle(
+                fontSize: 30.0,
+                color: _toggle ? Colors.blue : Colors.red,
+              ),
+              recognizer: _tapGestureRecognizer
+                ..onTap = () {
+                  setState(() {
+                    _toggle = !_toggle;
+                  });
+                },
+            ),
+            TextSpan(text: "你好世界"),
+          ],
+        ),
       ),
     );
   }
