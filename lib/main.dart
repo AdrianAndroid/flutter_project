bfaf5ac99aa643ef9e28ff633ca3803b507058e6
zhaojian2 <zhaojian2@joyy.com>
2022-02-24 10:01:52 +0800

    ExpansionTile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+const CITY_NAMES = {
+  '北京': ['东城区', '西城区', '海淀区', '朝阳区', '石景山区', '顺义区'],
+  '上海': ['黄浦区', '徐汇区', '长宁区', '静安区', '普陀区', '闸北区'],
+  '广州': ['越秀', '海珠', '荔湾', '天河', '白云', '黄埔', '南沙'],
+  '深圳': ['南山', '福田', '罗湖', '盐田', '龙岗', '宝安', '龙华'],
+  '杭州': ['上城区', '下城区', '江干区', '拱墅区', '西湖区', '滨江区'],
+  '苏州': ['姑苏区', '吴中区', '相城区', '高新区', '虎丘区', '工业园区', '吴江区'],
+};
 
 void main() => runApp(MyApp());
 
@@ -10,53 +18,45 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Listener',
       home: Scaffold(
-        appBar: AppBar(title: Text('手势检测')),
-        body: _GestureRecognizer(),
+        appBar: AppBar(title: Text('列表展开与收起')),
+        body: Container(
+          child: ListView(children: _buildList()),
+        ),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
-}
 
-class _GestureRecognizer extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _GestureRecognizerState();
-}
-
-class _GestureRecognizerState extends State<_GestureRecognizer> {
-  TapGestureRecognizer _tapGestureRecognizer = TapGestureRecognizer();
-  bool _toggle = false; //变色开关
-
-  @override
-  void dispose() {
-    // 用到GestureRecognizer的话一定要调用其dispose方法释放资源
-    _tapGestureRecognizer.dispose();
-    super.dispose();
+  List<Widget> _buildList() {
+    List<Widget> widgets = [];
+    CITY_NAMES.keys.forEach((key) {
+      widgets.add(_item(key, CITY_NAMES[key]));
+    });
+    return widgets;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text.rich(
-        TextSpan(
-          children: [
-            TextSpan(text: "你好世界"),
-            TextSpan(
-              text: "点我变色",
-              style: TextStyle(
-                fontSize: 30.0,
-                color: _toggle ? Colors.blue : Colors.red,
-              ),
-              recognizer: _tapGestureRecognizer
-                ..onTap = () {
-                  setState(() {
-                    _toggle = !_toggle;
-                  });
-                },
-            ),
-            TextSpan(text: "你好世界"),
-          ],
-        ),
+  Widget _item(String city, List<String>? subCities) {
+    List<Widget> list =
+        subCities?.map((subCity) => _buildSub(subCity))?.toList() ?? [];
+    return ExpansionTile(
+      title: Text(
+        city,
+        style: TextStyle(color: Colors.black54, fontSize: 20),
+      ),
+      children: list,
+    );
+  }
+
+  Widget _buildSub(String subCity) {
+    // 可以设置撑满宽度的盒子 称之为百分百布局
+    return FractionallySizedBox(
+      // 宽度因子 1为百分百撑满
+      widthFactor: 1,
+      child: Container(
+        height: 50,
+        margin: EdgeInsets.only(bottom: 5),
+        decoration: BoxDecoration(color: Colors.lightBlueAccent),
+        child: Text(subCity),
       ),
     );
   }
