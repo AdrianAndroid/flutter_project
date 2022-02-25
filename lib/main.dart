d9fab8f3c2abb6d97cf423db6ca15a53e7c13467
zhaojian2 <zhaojian2@joyy.com>
2022-02-25 10:58:10 +0800

    能够显示banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/banner.dart';
 
-const CITY_NAMES = {
-  '北京': ['东城区', '西城区', '海淀区', '朝阳区', '石景山区', '顺义区'],
-  '上海': ['黄浦区', '徐汇区', '长宁区', '静安区', '普陀区', '闸北区'],
-  '广州': ['越秀', '海珠', '荔湾', '天河', '白云', '黄埔', '南沙'],
-  '深圳': ['南山', '福田', '罗湖', '盐田', '龙岗', '宝安', '龙华'],
-  '杭州': ['上城区', '下城区', '江干区', '拱墅区', '西湖区', '滨江区'],
-  '苏州': ['姑苏区', '吴中区', '相城区', '高新区', '虎丘区', '工业园区', '吴江区'],
-};
+//https://www.wanandroid.com/banner/json
 
 void main() => runApp(MyApp());
 
@@ -16,47 +10,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Listener',
+      title: 'Banner',
       home: Scaffold(
-        appBar: AppBar(title: Text('列表展开与收起')),
-        body: Container(
-          child: ListView(children: _buildList()),
-        ),
+        appBar: AppBar(title: Text('Banner')),
+        body: Container(child: BannerPage()),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
+}
 
-  List<Widget> _buildList() {
-    List<Widget> widgets = [];
-    CITY_NAMES.keys.forEach((key) {
-      widgets.add(_item(key, CITY_NAMES[key]));
-    });
-    return widgets;
+class BannerPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BannerPage();
+}
+
+class _BannerPage extends State<BannerPage> {
+  List<BannerItem> datas = [];
+
+  @override
+  void initState() {
+    getHomeData();
+    super.initState();
   }
 
-  Widget _item(String city, List<String>? subCities) {
-    List<Widget> list =
-        subCities?.map((subCity) => _buildSub(subCity))?.toList() ?? [];
-    return ExpansionTile(
-      title: Text(
-        city,
-        style: TextStyle(color: Colors.black54, fontSize: 20),
+  getHomeData() async {
+    datas.addAll([
+      BannerItem.defaultBannerItem(
+        'https://www.wanandroid.com/blogimgs/50c115c2-cf6c-4802-aa7b-a4334de444cd.png',
+        '一起来做个App吧',
       ),
-      children: list,
-    );
+      BannerItem.defaultBannerItem(
+        'https://www.wanandroid.com/blogimgs/62c1bd68-b5f3-4a3c-a649-7ca8c7dfabe6.png',
+        '我们新增了一个常用导航Tab~',
+      ),
+      BannerItem.defaultBannerItem(
+        'https://www.wanandroid.com/blogimgs/90c6cc12-742e-4c9f-b318-b912f163b8d0.png',
+        '"flutter 中文社区',
+      ),
+    ]);
+    setState(() {});
   }
 
-  Widget _buildSub(String subCity) {
-    // 可以设置撑满宽度的盒子 称之为百分百布局
-    return FractionallySizedBox(
-      // 宽度因子 1为百分百撑满
-      widthFactor: 1,
-      child: Container(
-        height: 50,
-        margin: EdgeInsets.only(bottom: 5),
-        decoration: BoxDecoration(color: Colors.lightBlueAccent),
-        child: Text(subCity),
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          BannerWidget(
+            180.0,
+            datas,
+            bannerPress: (pos, item) {
+              print('bannerPress pos=>$pos, item=$item');
+            },
+          ),
+        ],
       ),
     );
   }
