0478bfe1be62306b6795f9776fdd15aa9c2fa6cd
zhaojian2 <zhaojian2@joyy.com>
2022-02-25 13:44:49 +0800

    Slidable

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_project/banner.dart';
-
-//https://www.wanandroid.com/banner/json
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,58 +11,97 @@ class MyApp extends StatelessWidget {
       title: 'Banner',
       home: Scaffold(
         appBar: AppBar(title: Text('Banner')),
-        body: Container(child: BannerPage()),
+        body: Container(
+          child: SlidablePage(),
+        ),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class BannerPage extends StatefulWidget {
+class SlidablePage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _BannerPage();
+  State<StatefulWidget> createState() => _SlidablePageState();
 }
 
-class _BannerPage extends State<BannerPage> {
-  List<BannerItem> datas = [];
-
+class _SlidablePageState extends State<SlidablePage> {
   @override
   void initState() {
-    getHomeData();
     super.initState();
-  }
-
-  getHomeData() async {
-    datas.addAll([
-      BannerItem.defaultBannerItem(
-        'https://www.wanandroid.com/blogimgs/50c115c2-cf6c-4802-aa7b-a4334de444cd.png',
-        '一起来做个App吧',
-      ),
-      BannerItem.defaultBannerItem(
-        'https://www.wanandroid.com/blogimgs/62c1bd68-b5f3-4a3c-a649-7ca8c7dfabe6.png',
-        '我们新增了一个常用导航Tab~',
-      ),
-      BannerItem.defaultBannerItem(
-        'https://www.wanandroid.com/blogimgs/90c6cc12-742e-4c9f-b318-b912f163b8d0.png',
-        '"flutter 中文社区',
-      ),
-    ]);
-    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          BannerWidget(
-            180.0,
-            datas,
-            bannerPress: (pos, item) {
-              print('bannerPress pos=>$pos, item=$item');
-            },
-          ),
-        ],
+    return Container(
+      child: Center(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.grey,
+              child: Slidable(
+                // Specify a key if the Slidable is dismissible
+                key: const ValueKey(0),
+                // The start action pane is the one at the left or the top side
+                startActionPane: ActionPane(
+                  // A motion is a widget used to control how the pane animates
+                  motion: const ScrollMotion(),
+                  // A pane can dismiss the Slidable
+                  dismissible: DismissiblePane(onDismissed: () {
+                    print('DismissiblePane');
+                  }),
+                  // All actions are defined in the children parameter.
+                  children: [
+                    // A SlidableAction can have a icon and/or a label.
+                    SlidableAction(
+                      backgroundColor: Color(0xFFFE4A49),
+                      foregroundColor: Colors.white,
+                      icon: Icons.delete,
+                      label: 'Delete',
+                      onPressed: (context) {
+                        print('Slidableaction onPressed: Delete');
+                      },
+                    ),
+                    SlidableAction(
+                      onPressed: (context) {
+                        print('Slidableaction onPressed: Share');
+                      },
+                      backgroundColor: Color(0xFF21B7CA),
+                      foregroundColor: Colors.white,
+                      icon: Icons.share,
+                      label: 'Share',
+                    ),
+                  ],
+                ),
+                endActionPane: ActionPane(
+                  motion: ScrollMotion(),
+                  children: [
+                    SlidableAction(
+                      flex: 2,
+                      onPressed: (context) {
+                        print('Slidableaction onPressed: Archive');
+                      },
+                      backgroundColor: Color(0xFF7BC043),
+                      foregroundColor: Colors.white,
+                      icon: Icons.archive,
+                      label: 'Archive',
+                    ),
+                    SlidableAction(
+                      onPressed: (context) {
+                        print('Slidableaction onPressed: Save');
+                      },
+                      backgroundColor: Color(0xFF0392CF),
+                      foregroundColor: Colors.white,
+                      icon: Icons.save,
+                      label: 'Save',
+                    ),
+                  ],
+                ),
+                child: const ListTile(title: Text('Slideme')),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
