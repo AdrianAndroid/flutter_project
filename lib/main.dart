3dca9c6a633f52fe2e566d4cf6f1d023b3dd91cb
zhaojian2 <zhaojian2@joyy.com>
2022-03-04 15:40:27 +0800

    添加几个button

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ void main() {
   runApp(MyApp());
 }
 
+// Flutter TextButton删除填充和内部填充 [英] Flutter TextButton Remove Padding and
+// Inner Padding
+// https://www.it1352.com/2255140.html
+
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -19,12 +24,83 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             _buildButton2(),
+            SizedBox(height: 10),
             _buildButton3(),
+            SizedBox(height: 5),
+            _buildButton4(context),
+            SizedBox(height: 5),
+            _buttonButton5(context),
+            SizedBox(height: 5),
+            _buttonButton6(),
+            SizedBox(height: 5),
+            _buttonButton7(),
           ],
         ),
       ),
     );
   }
+
+  _buttonButton7() => TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+          minimumSize: Size(50, 30),
+          alignment: Alignment.centerLeft,
+        ),
+        onPressed: () {},
+        child: Row(
+          children: [
+            Icon(
+              Icons.backpack,
+              size: 18,
+            ),
+            Text('Hello World!'),
+            Text(' Other'),
+          ],
+        ),
+      );
+
+  _buttonButton6() => OutlinedButton(
+        onPressed: () {},
+        style: OutlinedButton.styleFrom(
+          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          padding: EdgeInsets.only(top: 0, bottom: 0, right: 5, left: 5),
+          side: BorderSide(
+            width: 0.5,
+            color: Colors.black54,
+          ),
+          primary: Colors.white.withOpacity(0.9),
+        ),
+        child: Text("Hello World!", style: TextStyle(color: Colors.black87)),
+      );
+
+  _buttonButton5(BuildContext context) => ButtonTheme(
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        height: 10,
+        minWidth: 15,
+        padding: EdgeInsets.only(top: 5, bottom: 5, right: 5, left: 5),
+        child: FlatButton(
+          color: Colors.white.withOpacity(0.9),
+          onPressed: () {},
+          child: Text('Hello World'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            side: BorderSide(
+              color: Colors.black87,
+              width: 0.5,
+            ),
+          ),
+        ),
+      );
+
+  _buildButton4(BuildContext context) => TextButton.icon(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.all(0),
+          backgroundColor: Colors.black26,
+        ),
+        onPressed: () {},
+        icon: Icon(Icons.share, color: Theme.of(context).primaryColor),
+        label: Text('Hello World!'),
+      );
 
   _buildButton2() => Container(
         alignment: Alignment.topLeft,
@@ -33,8 +109,9 @@ class MyApp extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.green,
           //每条列表项底部加一个边框
-          border:
-              Border(bottom: BorderSide(width: 0.5, color: Color(0xFFd9d9d9))),
+          border: Border(
+            bottom: BorderSide(width: 0.5, color: Color(0xFFd9d9d9)),
+          ),
         ),
         child: FlatButton(
           // padding: new EdgeInsets.only(right: 200),
@@ -50,13 +127,13 @@ class MyApp extends StatelessWidget {
   _buildButton3() => Row(
         children: <Widget>[
           Expanded(
-            child: FlatButton(
-              color: Colors.blueGrey,
+            child: TextButton(
+              // color: Colors.blueGrey,
               child: Container(
                 alignment: Alignment.topLeft,
                 child: Text(
                   "A. 海洋生态系统",
-                  style: TextStyle(fontSize: 22.0, color: Colors.white),
+                  style: TextStyle(fontSize: 22.0, color: Colors.black),
                   textAlign: TextAlign.start,
                   maxLines: 1,
                 ),
