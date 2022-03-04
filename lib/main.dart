1692a8bd29bad02b86a5858a737414a370d5fbbd
zhaojian2 <zhaojian2@joyy.com>
2022-03-04 16:08:03 +0800

    没有生成虚线的button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:core';
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
 // 看这个资料
@@ -124,9 +125,126 @@ class _MyAppState extends State<MyApp> {
                     ],
                   ),
                 ),
+                SizedBox(height: 2),
+                Text('Hello World!'),
+                SizedBox(height: 2),
+                Container(
+                  color: Colors.grey,
+                  width: 100,
+                  height: 50,
+                  child: DashRect(
+                    color: Colors.red,
+                    strokeWidth: 2.0,
+                    gap: 3.0,
+                  ),
+                ),
+                SizedBox(height: 2),
+                Text('Hello World!'),
               ],
             ),
           ),
         ),
       );
+}
+
+class DashRect extends StatelessWidget {
+  final Color color;
+  final double strokeWidth;
+  final double gap;
+
+  const DashRect({
+    Key? key,
+    required this.color,
+    required this.strokeWidth,
+    required this.gap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class DashRectPainter extends CustomPainter {
+  final double strokeWidth;
+  final Color color;
+  final double gap;
+
+  DashRectPainter(this.strokeWidth, this.color, this.gap);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint dashedPaint = Paint()
+      ..color = color
+      ..strokeWidth = strokeWidth
+      ..style = PaintingStyle.stroke;
+
+    double x = size.width;
+    double y = size.height;
+
+    Path _topPath = getDashedPath(
+      a: math.Point(0, 0),
+      b: math.Point(x, 0),
+      gap: gap,
+    );
+
+    Path _rightPath = getDashedPath(
+      a: math.Point(0, y),
+      b: math.Point(x, y),
+      gap: gap,
+    );
+
+    Path _bottomPath = getDashedPath(
+      a: math.Point(0, y),
+      b: math.Point(0.001, y),
+      gap: gap,
+    );
+
+    Path _leftPath = getDashedPath(
+      a: math.Point(0, 0),
+      b: math.Point(0.001, y),
+      gap: gap,
+    );
+
+    canvas.drawPath(_topPath, dashedPaint);
+    canvas.drawPath(_rightPath, dashedPaint);
+    canvas.drawPath(_bottomPath, dashedPaint);
+    canvas.drawPath(_leftPath, dashedPaint);
+  }
+
+  Path getDashedPath({
+    required math.Point<double> a,
+    required math.Point<double> b,
+    required gap,
+  }) {
+    Size size = Size(b.x - a.x, b.y - a.y);
+    Path path = Path();
+    path.moveTo(a.x, a.y);
+    bool shouldDraw = true;
+    math.Point currentPoint = math.Point(a.x, a.y);
+
+    num radians = math.atan(size.height / size.width);
+
+    num dx = math.cos(radians) * gap < 0
+        ? math.cos(radians) * gap - 1
+        : math.cos(radians) * gap;
+
+    num dy = math.sin(radians) * gap < 0
+        ? math.sin(radians) * gap * -1
+        : math.sin(radians) * gap;
+    while (currentPoint.x <= b.x && currentPoint.y <= b.y) {
+      shouldDraw
+          ? path.lineTo(currentPoint.x.toDouble(), currentPoint.y.toDouble())
+          : path.moveTo(currentPoint.x.toDouble(), currentPoint.y.toDouble());
+      shouldDraw = !shouldDraw;
+      currentPoint = math.Point(
+        currentPoint.x + dx,
+        currentPoint.y + dy,
+      );
+    }
+    return path;
+  }
+
+  @override
+  bool shouldRepaint(covariant DashRectPainter oldDelegate) => true;
 }
