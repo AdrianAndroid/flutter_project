0cf2e2b07e2faa31d6f21ce7a95bac25c18fe9a4
zhaojian2 <zhaojian2@joyy.com>
2022-03-04 17:03:03 +0800

    原始指针事件

--- lib/banner.dart
+++ lib/banner.dart
@@ -6,6 +6,12 @@ import 'package:transparent_image/transparent_image.dart';
 
 // https://blog.csdn.net/qq939782569/article/details/111035348
 // 参考链接
+//https://www.wanandroid.com/blogimgs/50c115c2-cf6c-4802-aa7b-a4334de444cd.png
+//https://www.wanandroid.com/blogimgs/90c6cc12-742e-4c9f-b318-b912f163b8d0.png
+//https://www.wanandroid.com/blogimgs/ab17e8f9-6b79-450b-8079-0f2287eb6f0f.png
+//https://www.wanandroid.com/blogimgs/fb0ea461-e00a-482b-814f-4faca5761427.png
+//https://www.wanandroid.com/blogimgs/62c1bd68-b5f3-4a3c-a649-7ca8c7dfabe6.png
+//https://www.wanandroid.com/blogimgs/00f83f1d-3c50-439f-b705-54a49fc3d90d.jpg
 
 const MAX_COUNT = 0x7fffffff;
 
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,13 @@ import 'package:flutter_project/banner.dart';
 
 //https://www.wanandroid.com/banner/json
 
+//https://www.wanandroid.com/blogimgs/50c115c2-cf6c-4802-aa7b-a4334de444cd.png
+//https://www.wanandroid.com/blogimgs/90c6cc12-742e-4c9f-b318-b912f163b8d0.png
+//https://www.wanandroid.com/blogimgs/ab17e8f9-6b79-450b-8079-0f2287eb6f0f.png
+//https://www.wanandroid.com/blogimgs/fb0ea461-e00a-482b-814f-4faca5761427.png
+//https://www.wanandroid.com/blogimgs/62c1bd68-b5f3-4a3c-a649-7ca8c7dfabe6.png
+//https://www.wanandroid.com/blogimgs/00f83f1d-3c50-439f-b705-54a49fc3d90d.jpg
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,59 +20,43 @@ class MyApp extends StatelessWidget {
       title: 'Banner',
       home: Scaffold(
         appBar: AppBar(title: Text('Banner')),
-        body: Container(child: BannerPage()),
+        body: Container(child: MainRoute()),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class BannerPage extends StatefulWidget {
+class MainRoute extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _BannerPage();
+  State<StatefulWidget> createState() => _MainState();
 }
 
-class _BannerPage extends State<BannerPage> {
-  List<BannerItem> datas = [];
-
-  @override
-  void initState() {
-    getHomeData();
-    super.initState();
-  }
-
-  getHomeData() async {
-    datas.addAll([
-      BannerItem.defaultBannerItem(
-        'https://www.wanandroid.com/blogimgs/50c115c2-cf6c-4802-aa7b-a4334de444cd.png',
-        '一起来做个App吧',
-      ),
-      BannerItem.defaultBannerItem(
-        'https://www.wanandroid.com/blogimgs/62c1bd68-b5f3-4a3c-a649-7ca8c7dfabe6.png',
-        '我们新增了一个常用导航Tab~',
-      ),
-      BannerItem.defaultBannerItem(
-        'https://www.wanandroid.com/blogimgs/90c6cc12-742e-4c9f-b318-b912f163b8d0.png',
-        '"flutter 中文社区',
-      ),
-    ]);
-    setState(() {});
-  }
+class _MainState extends State<MainRoute> {
+  // 定义一个状态， 保存当前指针位置
+  PointerEvent? _event;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          BannerWidget(
-            180.0,
-            datas,
-            bannerPress: (pos, item) {
-              print('bannerPress pos=>$pos, item=$item');
-            },
-          ),
-        ],
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Listener(
+      child: Container(
+        color: Colors.grey,
+        alignment: Alignment.center,
+        width: width - 10,
+        height: height - 100,
+        child: Text(_event?.toString() ?? ""),
       ),
+      onPointerDown: (PointerDownEvent event) => setState(() {
+        _event = event;
+      }),
+      onPointerMove: (PointerMoveEvent event) => setState(() {
+        _event = event;
+      }),
+      onPointerUp: (PointerUpEvent event) => setState(() {
+        _event = event;
+      }),
     );
   }
 }
