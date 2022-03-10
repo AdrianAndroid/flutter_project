cf32ff6c62bc670bd357a9def900166e04bd6c28
zhaojian2 <zhaojian2@joyy.com>
2022-03-10 15:18:29 +0800

    自己写的

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,58 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text('AppBar')),
-        body: Center(child: Text('Hello World!')),
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                '我是设置透明度下面的Widget...',
+                style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 36.0,
+                  decoration: TextDecoration.lineThrough,
+                  shadows: [
+                    Shadow(
+                      color: Colors.red,
+                      offset: Offset(1, 1),
+                      blurRadius: 1.9,
+                    ),
+                    Shadow(
+                      color: Colors.black,
+                      offset: Offset(0, 1),
+                      blurRadius: 1.9,
+                    ),
+                    Shadow(
+                      color: Colors.blue,
+                      offset: Offset(0.5, 0.5),
+                      blurRadius: 1.9,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 100),
+              Opacity(
+                opacity: 0.6,
+                child: Container(
+                  color: Colors.black,
+                  padding: EdgeInsets.all(16.0),
+                  alignment: Alignment.bottomCenter,
+                  child: Column(
+                    children: [
+                      Text(
+                        '我是设置透明度上面的Widget...',
+                        style: TextStyle(
+                          color: Colors.orange,
+                          fontSize: 36.0,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
