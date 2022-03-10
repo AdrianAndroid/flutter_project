300a5b810f98dfb318a62407c818879711cab170
zhaojian2 <zhaojian2@joyy.com>
2022-03-10 15:57:34 +0800

    滚动监听

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,44 @@ import 'package:flutter/material.dart';
 // 给Flutter中的Widget设置透明度
 // https://www.jianshu.com/p/c87e147314a4
 
+// 滚动监听
+// NotificationListener监听滚动事件和ScrollController区别
+// 1，通过NotificationListener可以在从可滚动组件到widget树根之间任意位置都能监听。
+//  而ScrollController只能和具体的可滚动组件关联后才可以。
+// 2，收到滚动事件后获得的信息不同；NotificationListener在收到滚动事件时，
+//  通知中会携带当前滚动位置和ViewPort的一些信息，而ScrollController只能获取当前滚动位置。
+
 void main() {
-  runApp(MaterialApp(home: ControllerRoute()));
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  bool first = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('ScrollController')),
+      body: Stack(
+        children: [
+          first ? ControllerRoute() : ScrollNotificationRoute(),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                first = !first;
+              });
+            },
+            child: Text('Toggle'),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class ControllerRoute extends StatefulWidget {
@@ -43,7 +79,6 @@ class ControllerRouteState extends State<ControllerRoute> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ScrollController')),
       body: Scrollbar(
         child: ListView.builder(
           itemBuilder: (context, index) {
@@ -67,6 +102,60 @@ class ControllerRouteState extends State<ControllerRoute> {
               child: Icon(Icons.arrow_upward),
             )
           : null,
+    );
+  }
+}
+
+// 监听ListView滚动通知
+class ScrollNotificationRoute extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ScrollNotificationRouteState();
+}
+
+class ScrollNotificationRouteState extends State<ScrollNotificationRoute> {
+  String progress = "0%";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("滑动")),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Scrollbar(
+          child: NotificationListener<ScrollNotification>(
+            onNotification: (ScrollNotification notification) {
+              double pro = notification.metrics.pixels /
+                  notification.metrics.maxScrollExtent;
+              setState(() {
+                progress = '${(pro * 100).toInt()}%';
+              });
+              return true;
+            },
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                ListView.builder(
+                  itemCount: 100,
+                  itemExtent: 50,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        'zzz _ $index',
+                        style: TextStyle(wordSpacing: 3),
+                      ),
+                    );
+                  },
+                ),
+                CircleAvatar(
+                  child: Text(progress),
+                  backgroundColor: Colors.blueAccent,
+                  radius: 30,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
