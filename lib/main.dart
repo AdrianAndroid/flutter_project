9ba431769ecb09853e4610e23dfcb1223a13475e
zhaojian2 <zhaojian2@joyy.com>
2022-03-11 09:48:42 +0800

    去掉一些无用的东西

--- lib/main.dart
+++ lib/main.dart
@@ -9,43 +9,25 @@ void main() {
   runApp(MyApp());
 }
 
+class CountTime {
+  final String hour;
+  final String minute;
+  final String seconds;
+
+  CountTime({this.hour = '00', this.minute = '00', this.seconds = '00'});
+}
+
 class Utils {
-// 时间格式化，根据总秒数转换为对应的hh:mm:ss格式
-  static String constructTime(int seconds) {
-    int day = seconds ~/ 3600 ~/ 24;
+  // 时间格式化，根据总秒数转换为对应的hh:mm:ss格式
+  static CountTime constructTime2HMS(int seconds) {
+    // int day = seconds ~/ 3600 ~/ 24;
     int hour = seconds ~/ 3600;
     int minute = seconds % 3600 ~/ 60;
     int second = seconds % 60;
-
-    if (day != 0) {
-      return '$day天$hour小时$minute分$second秒后自动取消';
-    } else if (hour != 0) {
-      return '$hour小时$minute分$second秒后自动取消';
-    } else if (minute != 0) {
-      return '$minute分$second秒后自动取消';
-    } else if (second != 0) {
-      return '$second秒后自动取消';
-    } else {
-      return '';
-    }
-    //    return formatTime(day)+'天'+formatTime(hour) + "小时" + formatTime(minute) + "分" + formatTime(second)+'秒后自动取消';
-  }
-
-  static String constructVipTime(int seconds) {
-    int day = seconds ~/ 3600 ~/ 24;
-    int hour = seconds ~/ 3600;
-    int minute = seconds % 3600 ~/ 60;
-    int second = seconds % 60;
-    if (day != 0) {
-      return '剩$day天$hour小时$minute分';
-    } else if (hour != 0) {
-      return '剩$hour小时$minute分';
-    } else if (minute != 0) {
-      return '剩$minute分';
-    } else {
-      return '';
-    }
-//    return formatTime(day)+'天'+formatTime(hour) + "小时" + formatTime(minute) + "分" + formatTime(second)+'秒后自动取消';
+    return CountTime(
+        hour: formatTime(hour),
+        minute: formatTime(minute),
+        seconds: formatTime(second));
   }
 
   //数字格式化，将 0~9 的时间转换为 00~09
@@ -66,26 +48,73 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text('带时间格式的倒计时')),
-        body: OrderPage(),
+        body: CountDownWidget(
+          endCallback: () {
+            print('endCallback!!!');
+          },
+        ),
       ),
     );
   }
 }
 
-class OrderPage extends StatefulWidget {
+class CountDownWidget extends StatefulWidget {
+  final VoidCallback? endCallback;
+
+  const CountDownWidget({Key? key, this.endCallback}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() => OrderPageState();
+  State<StatefulWidget> createState() => _CountDownWidgetState();
 }
 
-class OrderPageState extends State<OrderPage> {
-  String countContent = ''; // 倒计时内容
+class _CountDownWidgetState extends State<CountDownWidget> {
+  CountTime? countTime; // 倒计时内容
   Timer? _timer;
-  int seconds = 5000;
+  int seconds = 10;
+  TextStyle style = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    color: Colors.white,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Text('$countContent');
+    return countTime != null ? _styleOne() : Container();
   }
+
+  _styleOne() => Row(
+        children: [
+          Container(
+            color: Color(0xFF242833),
+            alignment: Alignment.center,
+            width: 24,
+            height: 24,
+            child: Text(countTime!.hour, style: style),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 4, right: 4),
+            child: Text(':'),
+          ),
+          Container(
+            color: Color(0xFF242833),
+            alignment: Alignment.center,
+            width: 24,
+            height: 24,
+            child: Text(countTime!.minute, style: style),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 4, right: 4),
+            child: Text(':'),
+          ),
+          Container(
+            color: Color(0xFF242833),
+            alignment: Alignment.center,
+            width: 24,
+            height: 24,
+            child: Text(countTime!.seconds, style: style),
+          ),
+        ],
+      );
 
   void cancelTimer() {
     if (_timer != null) {
@@ -98,12 +127,16 @@ class OrderPageState extends State<OrderPage> {
     // 设置1秒回调一次
     const period = const Duration(seconds: 1);
     _timer = Timer.periodic(period, (timer) {
-      // 更新界面
-      setState(() {
-        // 秒数减一， 因为一秒回调一次
-        seconds--;
-        countContent = Utils.constructTime(seconds);
-      });
+      if (seconds < 0) {
+        cancelTimer();
+        widget.endCallback?.call();
+      } else {
+        // 更新界面
+        setState(() {
+          // 秒数减一， 因为一秒回调一次
+          countTime = Utils.constructTime2HMS(seconds--);
+        });
+      }
     });
   }
 
