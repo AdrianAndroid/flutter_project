7075630d6917134268c331a78f605791e2da6dc5
zhaojian2 <zhaojian2@joyy.com>
2022-03-15 16:55:06 +0800

    未知

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,11 @@ void main() {
   runApp(MyApp());
 }
 
+// 自恋情节
+
 class MyApp extends StatelessWidget {
+  final int Padding = 10;
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -16,8 +20,52 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text('AppBar')),
-        body: Center(child: Text('Hello World!')),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFFEBEBEB),
+                    borderRadius: BorderRadius.circular(150),
+                    border: Border.all(width: 1, color: Colors.black12),
+                  ),
+                  width: 150,
+                  height: 150,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Image.asset('images/lady.jpg'),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFEBEBEB),
+                    borderRadius: BorderRadius.circular(150),
+                    border: Border.all(width: 1, color: Colors.black12),
+                  ),
+                  width: 150,
+                  height: 150,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Container(
+                      child: Image.asset('images/lady.jpg'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
+
+  double _getInternalSide(double oldW) => oldW - Padding * 2;
 }
