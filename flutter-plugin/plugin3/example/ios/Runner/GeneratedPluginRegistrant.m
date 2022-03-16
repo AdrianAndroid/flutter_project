a94626f8e6bbc40a6622a90dae3a3fe868930462
zhaojian2 <zhaojian2@joyy.com>
2022-03-16 18:56:30 +0800

    plugin3剩下的文件

---
+++ flutter-plugin/plugin3/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<plugin3/Plugin3Plugin.h>)
+#import <plugin3/Plugin3Plugin.h>
+#else
+@import plugin3;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [Plugin3Plugin registerWithRegistrar:[registry registrarForPlugin:@"Plugin3Plugin"]];
+}
+
+@end
