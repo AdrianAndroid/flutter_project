245cd7a4d4808a520ca95250bbb964bb01c533bc
zhaojian2 <zhaojian2@joyy.com>
2022-03-17 18:13:37 +0800

    CarouselWithIndicatorDemo

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class CarouselDemo extends StatelessWidget {
           darkTheme: ThemeData.dark(),
           themeMode: ThemeMode.values.toList()[value as int],
           debugShowCheckedModeBanner: false,
-          home: OnDemandCarouseDemo(),
+          home: CarouselWithIndicatorDemo(),
         );
       },
     );
@@ -76,25 +76,66 @@ final List<Widget> imageSliders = imgList
         ))
     .toList();
 
+class CarouselWithIndicatorDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CarouselWithIndicatorDemoState();
+}
 
-class OnDemandCarouseDemo extends StatelessWidget {
+class _CarouselWithIndicatorDemoState extends State<CarouselWithIndicatorDemo> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('On-demand carousel demo')),
-      body: Container(
-        child: CarouselSlider.builder(
-          itemCount: 100,
-          itemBuilder: (ctx, index, realIdx) => Container(
-            child: Text(index.toString()),
-          ),
-          options: CarouselOptions(
-            aspectRatio: 2.0,
-            enlargeCenterPage: true,
-            autoPlay: true,
-          ),
-        ),
-      ),
+      appBar: AppBar(title: Text('Carousel with indicator controller demo')),
+      body: SafeArea(child: _buildBody()),
     );
   }
+
+  Widget _buildBody() => Column(
+        children: [
+          Expanded(
+            child: CarouselSlider(
+              items: imageSliders,
+              carouselController: _controller,
+              options: CarouselOptions(
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  aspectRatio: 2.0,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  }),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList
+                .asMap()
+                .entries
+                .map((entry) => GestureDetector(
+                      onTap: () => _controller.animateToPage(entry.key),
+                      child: Container(
+                        width: 12.0,
+                        height: 12.0,
+                        margin: EdgeInsets.symmetric(
+                          vertical: 8.0,
+                          horizontal: 4.0,
+                        ),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: (Theme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? Colors.white
+                                  : Colors.black)
+                              .withOpacity(_current == entry.key ? 0.9 : 0.4),
+                        ),
+                      ),
+                    ))
+                .toList(),
+          ),
+        ],
+      );
 }
