8e9cbaa7ecf2841771e07f5c44752182374c6755
zhaojian2 <zhaojian2@joyy.com>
2022-03-21 16:08:32 +0800

    ValueNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,52 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text('AppBar')),
-        body: Center(child: Text('Hello World!')),
+        body: ValueNotifierDemo(),
+      ),
+    );
+  }
+}
+
+class ValueNotifierDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ValueNotifierDemoState();
+}
+
+class _ValueNotifierDemoState extends State<ValueNotifierDemo> {
+  ValueNotifier<int> _valueNotifier = ValueNotifier(0);
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// 添加数据的变化监听
+    _valueNotifier.addListener(update);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    /// 组件销毁的时候注销监听
+    _valueNotifier.removeListener(update);
+  }
+
+  void update() {
+    print('数据变化监听回调');
+    // 刷新重建界面
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text(_valueNotifier.value.toString())),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // 修改值
+          _valueNotifier.value++;
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
