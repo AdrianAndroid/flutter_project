7b58fd20bd8fda0d75270020711d09f614de6a3e
zhaojian2 <zhaojian2@joyy.com>
2022-03-21 17:17:35 +0800

    《Flutter实战*第二版》6.10 CustomScrollView 和 Slivers

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 // 在Flutter中创建有意思的滚动效果 - Sliver系列
 // https://www.jianshu.com/p/5aeeb7ea776b
+
+// Flutter中常用的Sliver
+// Sliver名称	                功能	                          对应的可滚动组件
+// SliverList	                列表	                          ListView
+// SliverFixedExtentList	    高度固定的列表	                ListView，指定itemExtent时
+// SliverAnimatedList	        添加/删除列表项可以执行动画	      AnimatedList
+// SliverGrid	                网格	                          GridView
+// SliverPrototypeExtentList	根据原型生成高度固定的列表	      ListView，指定prototypeItem 时
+// SliverFillViewport	        包含多给子组件，每个都可以填满屏幕	PageView
+
+// 它们的子组件必须是Sliver
+// Sliver名称	                      对应 RenderBox
+// SliverPadding	                  Padding
+// SliverVisibility、SliverOpacity	Visibility、Opacity
+// SliverFadeTransition	            FadeTransition
+// SliverLayoutBuilder	            LayoutBuilder
+
+// 还有一些其它常用的Sliver
+// Sliver名称	            说明
+// SliverAppBar	          对应 AppBar，主要是为了在 CustomScrollView 中使用。
+// SliverToBoxAdapter	    一个适配器，可以将 RenderBox 适配为 Sliver，后面介绍。
+// SliverPersistentHeader	滑动到顶部时可以固定住，后面介绍。
 
 void main() => runApp(MyApp());
 
@@ -11,255 +32,126 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Woolha.com Flutter Tutorial',
-      home: CustomSliverHeaderDemo(),
+      home: Scaffold(
+        appBar: AppBar(title: Text("SliverList学习")),
+        body: SliverListFul(),
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class CustomSliverHeaderDemo extends StatelessWidget {
+class SliverListFul extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverPersistentHeader(
-            pinned: true,
-            delegate: SliverCustomHeaderDelegate(
-                title: '哪吒之魔童降世',
-                collapsedHeight: 40,
-                expandedHeight: 300,
-                paddingTop: MediaQuery.of(context).padding.top,
-                coverImgUrl:
-                    'https://img.zcool.cn/community/01c6615d3ae047a8012187f447cfef.jpg@1280w_1l_2o_100sh.jpg'),
-          ),
-          SliverFillRemaining(
-            child: FilmContent(),
-          )
-        ],
-      ),
-    );
-  }
+  // State<StatefulWidget> createState() => SliverListFulState();
+  State<StatefulWidget> createState() => SliverListFulState();
 }
 
-class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
-  final double collapsedHeight;
-  final double expandedHeight;
-  final double paddingTop;
-  final String coverImgUrl;
-  final String title;
-  String statusBarMode = 'dark';
-
-  SliverCustomHeaderDelegate({
-    required this.collapsedHeight,
-    required this.expandedHeight,
-    required this.paddingTop,
-    required this.coverImgUrl,
-    required this.title,
-  });
-
-  @override
-  double get minExtent => this.collapsedHeight + this.paddingTop;
-
-  @override
-  double get maxExtent => this.expandedHeight;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return true;
-  }
-
-  void updateStatusBarBrightness(shrinkOffset) {
-    if (shrinkOffset > 50 && this.statusBarMode == 'dark') {
-      this.statusBarMode = 'light';
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.light,
-        statusBarIconBrightness: Brightness.light,
-      ));
-    } else if (shrinkOffset <= 50 && this.statusBarMode == 'light') {
-      this.statusBarMode = 'dark';
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.dark,
-        statusBarIconBrightness: Brightness.dark,
-      ));
-    }
-  }
-
-  Color makeStickyHeaderBgColor(shrinkOffset) {
-    final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255)
-        .clamp(0, 255)
-        .toInt();
-    return Color.fromARGB(alpha, 255, 255, 255);
-  }
-
-  Color makeStickyHeaderTextColor(shrinkOffset, isIcon) {
-    if (shrinkOffset <= 50) {
-      return isIcon ? Colors.white : Colors.transparent;
-    } else {
-      final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255)
-          .clamp(0, 255)
-          .toInt();
-      return Color.fromARGB(alpha, 0, 0, 0);
-    }
-  }
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    this.updateStatusBarBrightness(shrinkOffset);
-    return Container(
-      height: this.maxExtent,
-      width: MediaQuery.of(context).size.width,
-      child: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(child: Image.network(this.coverImgUrl, fit: BoxFit.cover)),
-          Positioned(
-            left: 0,
-            top: this.maxExtent / 2,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color(0x00000000),
-                    Color(0x90000000),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            child: Container(
-              color: this.makeStickyHeaderBgColor(shrinkOffset),
-              child: SafeArea(
-                bottom: false,
-                child: Container(
-                  height: this.collapsedHeight,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(
-                        icon: Icon(
-                          Icons.arrow_back_ios,
-                          color: this
-                              .makeStickyHeaderTextColor(shrinkOffset, true),
-                        ),
-                        onPressed: () => Navigator.pop(context),
-                      ),
-                      Text(
-                        this.title,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: this
-                              .makeStickyHeaderTextColor(shrinkOffset, false),
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.share,
-                          color: this
-                              .makeStickyHeaderTextColor(shrinkOffset, true),
-                        ),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class FilmContent extends StatelessWidget {
+class SliverListFulState extends State<SliverListFul> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(6),
-                child: Image.network(
-                  'https://img1.gamersky.com/image2019/07/20190725_ll_red_136_2/gamersky_07small_14_201972510258D0.jpg',
-                  width: 130,
-                  height: 180,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(left: 16)),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '哪吒之魔童降世',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF333333),
-                    ),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 10)),
-                  Text(
-                    '动画/中国大陆/110分钟',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Color(0xFF999999),
-                    ),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 2)),
-                  Text(
-                    '32.1万人想看/大V推荐度95%',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Color(0xFF999999),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Divider(height: 32),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '剧情简介',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF333333),
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(top: 10)),
-              Text(
-                '天地灵气孕育出一颗能量巨大的混元珠，元始天尊将混元珠提炼成灵珠和魔丸，灵珠投胎为人，助周伐纣时可堪大用；而魔丸则会诞'
-                '出魔王，为祸人间。元始天尊启动了天劫咒语，3年后天雷将会降临，摧毁魔丸。太乙受命将灵珠托生于陈塘关李靖家的儿子哪吒身上。然而阴差阳错，灵珠和魔丸竟然被掉包。本应是灵珠英雄的哪吒却成了混世大魔王。调皮捣蛋顽劣不堪的哪吒却徒有一颗做英雄的心。然而面对众人对魔丸的误解和即将来临的天雷的降临，哪吒是否命中注定会立地成魔？他将何去何从？',
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Color(0xFF999999),
-                ),
-              ),
-            ],
-          ),
-        ],
+    return _buildSliverToBoxAdapter();
+  }
+
+  Widget buildTwoSliverList() {
+    // SliverFixedExtentList 是一个 Sliver，它可以生成高度相同的列表项。
+    // 再次提醒，如果列表项高度相同，我们应该优先使用SliverFixedExtentList
+    // 和 SliverPrototypeExtentList，如果不同，使用 SliverList.
+    var listView = SliverFixedExtentList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => ListTile(title: Text('$index')),
+        childCount: 10,
       ),
+      itemExtent: 56,
+    );
+    return CustomScrollView(
+      slivers: [
+        listView,
+        listView,
+      ],
+    );
+  }
+
+  /// 上下并列的ListView
+  Widget buildTwoListView() {
+    var listView = ListView.builder(
+      itemBuilder: (_, index) => ListTile(title: Text('$index')),
+      itemCount: 20,
+    );
+    return Column(
+      children: [
+        Expanded(child: listView),
+        Divider(),
+        Expanded(child: listView),
+      ],
+    );
+  }
+
+  Widget buildSliver() {
+    return CustomScrollView(
+      slivers: [
+        // AppBar, 包含一个导航栏
+        SliverAppBar(
+          pinned: true, // 滑动至顶端时会固定住
+          expandedHeight: 250.0,
+          flexibleSpace: FlexibleSpaceBar(
+            title: const Text('Demo'),
+            background: Image.asset('assets/sea.jpeg', fit: BoxFit.cover),
+          ),
+        ),
+        SliverPadding(
+          padding: EdgeInsets.all(8.0),
+          sliver: SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // Grid按两列显示
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 4.0,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => Container(
+                alignment: Alignment.center,
+                color: Colors.cyan[100 * (index % 9)],
+                child: Text('grid item $index'),
+              ),
+              childCount: 20,
+            ),
+          ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) => Container(
+              alignment: Alignment.center,
+              color: Colors.lightBlue[100 * (index % 9)],
+              child: Text('list item $index'),
+            ),
+            childCount: 20,
+          ),
+          itemExtent: 50.0,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSliverToBoxAdapter() {
+    var listView = SliverFixedExtentList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => ListTile(title: Text('$index')),
+        childCount: 20,
+      ),
+      itemExtent: 56,
+    );
+
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: SizedBox(
+            height: 300,
+            child: PageView(children: [Text('1'), Text('2')]),
+          ),
+        ),
+        listView
+      ],
     );
   }
 }
