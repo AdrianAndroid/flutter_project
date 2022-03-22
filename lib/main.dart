b055870bb342d4727999feaf773d266082a03c33
zhaojian2 <zhaojian2@joyy.com>
2022-03-22 18:14:11 +0800

    Offstage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// Visibility 隐藏/可见， 能保存组件的状态：Offstate不能保存组件的状态，组件重新加载控制child是否显示
+/// 当offstage为true，控件隐藏；当offstage为false， 显示；
+/// 当Offstage不可见的时候，如果child又动画等，需要手动停掉，Offstage并不会停掉动画等操作
+/// const Offstage({Key key, this.offstage = true, Widget child})
+
 void main() {
   runApp(MyApp());
 }
@@ -14,10 +19,54 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('AppBar')),
-        body: Center(child: Text('Hello World!')),
+      home: OffstageDemo(),
+    );
+  }
+}
+
+class OffstageDemo extends StatefulWidget {
+  @override
+  State<OffstageDemo> createState() => _OffstageDemoState();
+}
+
+class _OffstageDemoState extends State<OffstageDemo> {
+  String showText = 'hide';
+  bool isOff = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('AppBar')),
+      body: Container(
+        margin: EdgeInsets.only(top: 50.0),
+        child: Center(
+          child: Column(
+            children: [
+              Text('--------'),
+              Offstage(
+                offstage: isOff,
+                child: Text('Offstage组件'),
+              ),
+              Text('--------'),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: onButtonClickListener,
+        child: Text(showText),
       ),
     );
+  }
+
+  void onButtonClickListener() {
+    setState(() {
+      isOff = !isOff;
+    });
+    if (isOff) {
+      showText = 'show';
+    } else {
+      showText = 'hide';
+    }
   }
 }
