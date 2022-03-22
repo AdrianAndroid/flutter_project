c1b17767a838760c678a7281729daf38f7a43e02
zhaojian2 <zhaojian2@joyy.com>
2022-03-22 16:09:32 +0800

    drag_and_drop_lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,21 @@
+// import 'package:example/basic_example.dart';
+// import 'package:example/drag_handle_example.dart';
+// import 'package:example/drag_into_list_example.dart';
+// import 'package:example/expansion_tile_example.dart';
+// import 'package:example/fixed_example.dart';
+// import 'package:example/horizontal_example.dart';
+// import 'package:example/list_tile_example.dart';
+// import 'package:example/sliver_example.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/sliver_example.dart';
+
+import 'basic_example.dart';
+import 'drag_handle_example.dart';
+import 'drag_into_list_example.dart';
+import 'expansion_tile_example.dart';
+import 'fixed_example.dart';
+import 'horizontal_example.dart';
+import 'list_tile_example.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,15 +26,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Drag and Drop Lists',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('AppBar')),
-        body: Center(child: Text('Hello World!')),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => BasicExample(),
+        '/list_tile_example': (context) => ListTileExample(),
+        '/expansion_tile_example': (context) => ExpansionTileExample(),
+        '/sliver_example': (context) => SliverExample(),
+        '/horizontal_example': (context) => HorizontalExample(),
+        '/drag_into_list_example': (context) => DragIntoListExample(),
+        '/fixed_example': (context) => FixedExample(),
+        '/drag_handle_example': (context) => DragHandleExample(),
+      },
     );
   }
 }
