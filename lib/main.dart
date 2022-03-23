7ed8f0f3dea4b2cbbaf0e3e72ae1b21a27273e16
zhaojian2 <zhaojian2@joyy.com>
2022-03-23 10:15:37 +0800

    InkWell & Ink

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,56 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text('AppBar')),
-        body: Center(child: Text('Hello World!')),
+        body: Center(
+          child: Column(
+            children: [
+              InkWell(
+                onTap: () {},
+                splashColor: Colors.red,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text('这是InkWell点击效果'),
+                  height: 30,
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  child: Text('这是InkWell的点击效果'),
+                ),
+              ),
+              Ink(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color(0xFFDE2F21),
+                      Color(0xFFDE2F21),
+                    ],
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: InkWell(
+                  onTap: () {},
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                    child: Text(
+                      '这是InkWell的点击效果',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
