62c7e34e39286f5e69befa5fdd5378bf40f04e75
zhaojian2 <zhaojian2@joyy.com>
2022-03-24 13:56:53 +0800

    运行

---
+++ waterfall_flow/example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/flannery/.terra/caches/sdk/2.0.6/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/flannery/Desktop/mylibrary/flutter_project/waterfall_flow/example"
+export "FLUTTER_TARGET=/Users/flannery/Desktop/mylibrary/flutter_project/waterfall_flow/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/flannery/Desktop/mylibrary/flutter_project/waterfall_flow/example/.dart_tool/package_config.json"
